c4ad72d52124e0bdc150b48cec9c9e3c148dd167
sooraj888 <soorajsagar888@gmail.com>
2025-05-06 14:16:39 +0530

    feat: add 404 Not Found and Error pages

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { Button, Container, Typography } from "@mui/material";
 
-export default function ErrorPage() {
-  return <>ErrorPage</>;
-}
+const ErrorPage = () => (
+  <Container
+    sx={{
+      textAlign: "center",
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+    }}
+  >
+    <Typography variant="h4" color="error" sx={{ margin: "auto" }}>
+      Something went wrong!
+      <Button
+        sx={{ display: "block", margin: "10px auto" }}
+        variant="outlined"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+      >
+        Go Home
+      </Button>
+    </Typography>
+  </Container>
+);
+
+export default ErrorPage;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { Button, Container, Typography } from "@mui/material";
 
-export default function NotFound() {
-  return <>NotFound</>;
-}
+const NotFound = () => {
+  return (
+    <Container
+      sx={{
+        textAlign: "center",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+      }}
+    >
+      <Typography variant="h3" sx={{ margin: "auto" }}>
+        404 - Page Not Found
+        <Button
+          sx={{ display: "block", margin: "10px auto" }}
+          variant="outlined"
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          Go Home
+        </Button>
+      </Typography>
+    </Container>
+  );
+};
+
+export default NotFound;
